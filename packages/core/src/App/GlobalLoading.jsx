3f666de3009a161189189665db1a96a11ef39ab7
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2025-10-28 20:31:33 +0300

    Fix import path for LOGO in GlobalLoading.jsx
    Mumo

--- packages/core/src/App/GlobalLoading.jsx
+++ packages/core/src/App/GlobalLoading.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import LOGO from './LOGO/DELTA.png';
+import LOGO from './Logo/DELTA.png';
 import './GlobalLoading.scss';
 
 const GlobalLoading = () => {
